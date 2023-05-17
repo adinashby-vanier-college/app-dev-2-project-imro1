f484a5220cddfb0822d4d160e9779ad5071b82e1
Aliraza1002 <Aliraza_159@outlook.com>
2023-05-17 18:46:50 -0400

    API calls for flights and flight details work

--- aero_flights_app/lib/pages/flights_page.dart
+++ aero_flights_app/lib/pages/flights_page.dart
@@ -1,71 +1,149 @@
 import 'package:flutter/material.dart';
-import 'package:aero_flights/pages/home_page.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class FlightsPage extends StatelessWidget {
+class FlightsPage extends StatefulWidget {
   FlightsPage({Key? key}) : super(key: key);
 
-  final List<Map<String, dynamic>> _flights = [
-    {
-      'id': '1',
-      'airline': 'Delta',
-      'departure': 'New York',
-      'destination': 'Los Angeles',
-      'price': '250',
-      'time': '8:00 AM',
-    },
-    {
-      'id': '2',
-      'airline': 'United',
-      'departure': 'Los Angeles',
-      'destination': 'Chicago',
-      'price': '350',
-      'time': '10:00 AM',
-    },
-    {
-      'id': '3',
-      'airline': 'American Airlines',
-      'departure': 'Chicago',
-      'destination': 'New York',
-      'price': '200',
-      'time': '12:00 PM',
-    },
-    {
-      'id': '4',
-      'airline': 'Southwest',
-      'departure': 'Denver',
-      'destination': 'Los Angeles',
-      'price': '150',
-      'time': '9:00 AM',
-    },
-    {
-      'id': '5',
-      'airline': 'JetBlue',
-      'departure': 'New York',
-      'destination': 'Miami',
-      'price': '300',
-      'time': '11:00 AM',
-    },
-  ];
+  @override
+  _FlightsPageState createState() => _FlightsPageState();
+}
+
+class _FlightsPageState extends State<FlightsPage> {
+  List<Map<String, dynamic>> _flights = [];
+  Map<String, dynamic>? _selectedFlight;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchFlightData();
+  }
+
+  Future<void> fetchFlightData() async {
+    final apiKey = 'f8f4e34c1f6fba91e910e330b28ce41e';
+    final apiUrl = 'http://api.aviationstack.com/v1/flights';
+
+    final requestUrl = Uri.parse('$apiUrl?access_key=$apiKey');
+
+    try {
+      final response = await http.get(requestUrl);
+      if (response.statusCode == 200) {
+        final responseData = json.decode(response.body);
+        final List<dynamic>? flightsData = responseData['data'];
+
+        if (flightsData != null) {
+          setState(() {
+            _flights = flightsData
+                .map((flight) => {
+              'id': flight['flight']['number'],
+              'airline': flight['airline']['name'],
+              'departure': flight['departure']['airport'],
+              'destination': flight['arrival']['airport'],
+              'price': '199.99', // Placeholder value for price
+              'status': flight['status'],
+              'time': flight['departure']['estimated'],
+            })
+                .toList();
+          });
+        } else {
+          print('No flight data available.');
+        }
+      } else {
+        print('Error: ${response.statusCode}');
+      }
+    } catch (error) {
+      print('Request error: $error');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flights'),
+        backgroundColor: Colors.teal,
+        title: Text(
+          'Flights',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
       ),
-      body: ListView.builder(
+      backgroundColor: Colors.grey[100],
+      body: _selectedFlight != null
+          ? FlightDetailsPage(flight: _selectedFlight!)
+          : _flights.isEmpty
+          ? Center(
+        child: CircularProgressIndicator(),
+      )
+          : ListView.builder(
         itemCount: _flights.length,
         itemBuilder: (context, index) {
           final flight = _flights[index];
           return Padding(
             padding: const EdgeInsets.all(10.0),
-            child: Card(
-              child: ListTile(
-                title: Text('${flight['airline']} - ${flight['id']}'),
-                subtitle: Text(
-                  '${flight['departure']} to ${flight['destination']} at ${flight['time']}',
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => FlightDetailsPage(
+                      flight: flight,
+                    ),
+                  ),
+                );
+              },
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '${flight['airline']} - ${flight['id']}',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        '${flight['departure']} to ${flight['destination']}',
+                        style: TextStyle(fontSize: 16),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      SizedBox(height: 8),
+                      Row(
+                        mainAxisAlignment:
+                        MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Price: \$${flight['price']}',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      FlightDetailsPage(
+                                        flight: flight,
+                                      ),
+                                ),
+                              );
+                            },
+                            style: ElevatedButton.styleFrom(
+                                primary: Colors.teal),
+                            child: Text('Save'),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-                trailing: Text('\$${flight['price']}'),
               ),
             ),
           );
@@ -74,3 +152,78 @@ class FlightsPage extends StatelessWidget {
     );
   }
 }
+
+class FlightDetailsPage extends StatelessWidget {
+  final Map<String, dynamic> flight;
+
+  const FlightDetailsPage({Key? key, required this.flight}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal, // Set background color
+        title: Text(
+          'Flight Details',
+          style: TextStyle(
+            color: Colors.white, // Set text color
+          ),
+        ),
+      ),
+      backgroundColor: Colors.grey[100], // Set background color
+      body: _buildFlightDetails(context),
+    );
+  }
+
+  Widget _buildFlightDetails(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center, // Center the content vertically
+      children: [
+        Image.asset(
+          'images/flight.png', // Provide the path to your flight image
+          width: MediaQuery.of(context).size.width * 0.6, // Take 80% of the screen width
+        ),
+        SizedBox(height: 16),
+        Text(
+          '${flight['airline']} - ${flight['id']}',
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 8),
+        Text(
+          'Departure: ${flight['departure'] ?? 'Unknown'}',
+          style: TextStyle(fontSize: 16),
+        ),
+        SizedBox(height: 8),
+        Text(
+          'Destination: ${flight['destination'] ?? 'Unknown'}',
+          style: TextStyle(fontSize: 16),
+        ),
+        SizedBox(height: 8),
+        Text(
+          'Time: ${flight['time'] ?? 'Unknown'}',
+          style: TextStyle(fontSize: 16),
+        ),
+        SizedBox(height: 8),
+        Text(
+          'Price: \$${flight['price'] ?? 'Unknown'}',
+          style: TextStyle(fontSize: 16),
+        ),
+        SizedBox(height: 16),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context); // Go back to the previous route
+          },
+          style: ElevatedButton.styleFrom(primary: Colors.teal), // Set button color
+          child: Text('Back to Flights'),
+        ),
+      ],
+    );
+  }
+}
+
+
+
+
+
+
+
--- aero_flights_app/lib/pages/search_page.dart
+++ aero_flights_app/lib/pages/search_page.dart
@@ -1,26 +1,196 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'flights_page.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
+
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  List<Map<String, dynamic>> _flights = [];
+  List<Map<String, dynamic>> _searchResults = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchFlightData();
+  }
+
+  Future<void> fetchFlightData() async {
+    final apiKey = 'f8f4e34c1f6fba91e910e330b28ce41e';
+    final apiUrl = 'http://api.aviationstack.com/v1/flights';
+
+    final requestUrl = Uri.parse('$apiUrl?access_key=$apiKey');
+
+    try {
+      final response = await http.get(requestUrl);
+      if (response.statusCode == 200) {
+        final responseData = json.decode(response.body);
+        final List<dynamic>? flightsData = responseData['data'];
+
+        if (flightsData != null) {
+          setState(() {
+            _flights = flightsData
+                .map((flight) => {
+              'id': flight['flight']['number'],
+              'airline': flight['airline']['name'],
+              'departure': flight['departure']['airport'],
+              'destination': flight['arrival']['airport'],
+              'price': '199.99', // Placeholder value for price
+              'status': flight['status'],
+              'time': flight['departure']['estimated'],
+            })
+                .toList();
+          });
+        } else {
+          print('No flight data available.');
+        }
+      } else {
+        print('Error: ${response.statusCode}');
+      }
+    } catch (error) {
+      print('Request error: $error');
+    }
+  }
+
+  void searchFlights(String query) {
+    setState(() {
+      _searchResults = _flights
+          .where((flight) =>
+      flight['airline'].toString().toLowerCase().contains(query.toLowerCase()) ||
+          flight['departure'].toString().toLowerCase().contains(query.toLowerCase()) ||
+          flight['destination'].toString().toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Search'),
+        backgroundColor: Colors.teal,
       ),
+      backgroundColor: Colors.grey[100],
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
             SizedBox(height: 10),
             TextFormField(
+              onChanged: (value) => searchFlights(value),
               decoration: InputDecoration(
                 hintText: 'Search',
                 prefixIcon: Icon(Icons.search),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(10),
                 ),
+              ),
+              style: TextStyle(color: Colors.teal),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                // Button action
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.teal,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+              ),
+              child: Text(
+                'Search',
+                style: TextStyle(fontSize: 16, color: Colors.white),
+              ),
+            ),
+            SizedBox(height: 16),
+            Expanded(
+              child: _searchResults.isEmpty
+                  ? Center(
+                child: Text(
+                  'No flights available',
+                  style: TextStyle(fontSize: 16),
+                ),
+              )
+                  : ListView.builder(
+                itemCount: _searchResults.length,
+                itemBuilder: (context, index) {
+                  final flight = _searchResults[index];
+                  return Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => FlightDetailsPage(
+                              flight: flight,
+                            ),
+                          ),
+                        );
+                      },
+                      child: Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                '${flight['airline']} - ${flight['id']}',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(height: 8),
+                              Text(
+                                '${flight['departure']} to ${flight['destination']}',
+                                style: TextStyle(fontSize: 16),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              SizedBox(height: 8),
+                              Row(
+                                mainAxisAlignment:
+                                MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    'Price: \$${flight['price']}',
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  ElevatedButton(
+                                    onPressed: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) =>
+                                              FlightDetailsPage(
+                                                flight: flight,
+                                              ),
+                                        ),
+                                      );
+                                    },
+                                    style: ElevatedButton.styleFrom(
+                                        primary: Colors.teal),
+                                    child: Text('Save'),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
               ),
             ),
           ],
@@ -29,3 +199,4 @@ class SearchPage extends StatelessWidget {
     );
   }
 }
+
